38ee41ab92e5710fb14114ea56e77ca9b43e91f0
LuffyTaro008 <rizal83702@gmail.com>
2021-11-08 19:21:45 +0700

    anykernel.sh:Sunny-Go

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=D.kernel Redmi 5 (Rosy) by Dexter001 @ xda-developers
+kernel.string=Sunny-Go Redmi 5 (Rosy) by LuffyTaro008 @ xda-developers
 do.devicecheck=1
 do.modules=0
 do.systemless=1
